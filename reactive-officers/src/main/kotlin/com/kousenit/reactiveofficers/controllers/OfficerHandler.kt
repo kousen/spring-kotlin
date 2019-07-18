c065d173277b3716d7ca6f3013e36674d446a50e
Ken Kousen <ken.kousen@kousenit.com>
2019-07-18 12:37:51 -0600

    Upgrade gradle wrapper and minor changes

--- reactive-officers/src/main/kotlin/com/kousenit/reactiveofficers/controllers/OfficerHandler.kt
+++ reactive-officers/src/main/kotlin/com/kousenit/reactiveofficers/controllers/OfficerHandler.kt
@@ -7,7 +7,6 @@ import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.ServerRequest
 import org.springframework.web.reactive.function.server.ServerResponse
-import org.springframework.web.reactive.function.server.ServerResponse.noContent
 import org.springframework.web.reactive.function.server.body
 
 @Component
