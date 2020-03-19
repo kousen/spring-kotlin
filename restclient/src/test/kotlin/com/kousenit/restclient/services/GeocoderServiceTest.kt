e5565825513c9cc0a51baabe805329937fdcfa4c
Ken Kousen <ken.kousen@kousenit.com>
2020-03-19 19:19:01 -0400

    Add properties file for JUnit 5 per class

--- restclient/src/test/kotlin/com/kousenit/restclient/services/GeocoderServiceTest.kt
+++ restclient/src/test/kotlin/com/kousenit/restclient/services/GeocoderServiceTest.kt
@@ -4,6 +4,7 @@ import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.`is`
 import org.hamcrest.Matchers.closeTo
 import org.junit.jupiter.api.Test
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
