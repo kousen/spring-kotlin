bce1f0737a45503fc86395260b87d2cb1c66a6ff
Ken Kousen <ken.kousen@kousenit.com>
2021-04-28 22:05:12 -0400

    Upgrade versions

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.4.2"
+    id("org.springframework.boot") version "2.4.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
-    kotlin("jvm") version "1.4.21"
-    kotlin("plugin.spring") version "1.4.21"
+    kotlin("jvm") version "1.4.32"
+    kotlin("plugin.spring") version "1.4.32"
 }
 
 group = "com.kousenit"
